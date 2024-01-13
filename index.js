fb38c1c839ae846fe2aee971f1d9bf485d4b2184
Dale Warburton <dalewarby23@outlook.com>
2024-01-13 13:08:34 +0000

    finished questions and added validation for user input

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//modules required - fs already installed and inquirer required installing using npm
+//modules required - fs already installed just needs calling and inquirer required installing using npm v6.5.0
 const fs = require("fs");
 const inquirer = require("inquirer");
 
@@ -8,39 +8,126 @@ const generateMarkdown = require(".utils/generateMarkdown");
 //Array of questions saved as an arrow function  
 const questions = () => {
     //inquirer used to store questions for user
-   return inquirer
-    .prompt([
-        { 
-            type: 'input',
-            message: 'What is the title of your project? ',
-            name: 'title',
-        }, 
-        {
-            type: 'input',
-            message: 'What are the sections entitled? ',
-            name: 'entitled',
-        },
-        {
-            type: 'input',
-            message: 'Provide a description of your project: ',
-            name: 'description',
-        },
-        {
-            type: 'input',
-            message: 'Please relay your tabel of contents: ',
-            name: 'contents',
-        },
-        {
-            type: 'input',
-            message: 'What installations did you require? ',
-            name: 'installations',
-        }
-        {
-            type: 'input',
-            message: 'Describe the usage of your project? ',
-            name: 'usage',
-        }
-
-
-    ])
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the title of your project? ',
+                name: 'title',
+                // validation for the users input to return a true value and if not ask the user to re-enter the title
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the title of your project. ');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'What are the sections entitled? ',
+                name: 'entitled',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the sections of your project. ');
+                        return false;
+                    }
+                   
+                }
+            },    
+            {
+                type: 'input',
+                message: 'Provide a description of your project: ',
+                name: 'description',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a description of your project. ');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'Please relay your table of contents: ',
+                name: 'contents',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the contents of your project. ');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'What installations did you require? ',
+                name: 'installations',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a guide to installations. ');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'Describe the usage of your project? ',
+                name: 'usage',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the usage of your project. ');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'List',
+                message: 'What type of licence would you like to add to your project',
+                name: 'license',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please chose a license for your project. ');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'Please list any contributers to your project. ',
+                name: 'contributers',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please list contributers. ');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: 'What testing methods did you do? ',
+                name: 'test',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter testing methods used. ');
+                        return false;
+                    }
+                }
+            }
+        ])
 }
